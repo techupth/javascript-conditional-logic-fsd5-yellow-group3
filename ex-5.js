6d51a46b693904b3e32f118df7318c71ff5bc50a
doctor <fd5@MacBook-Air-2022.local>
2023-11-30 15:42:29 +0700

    edit ex5 v2

--- ex-5.js
+++ ex-5.js
@@ -13,11 +13,9 @@ let lisaGrade = calculateStudentGrade(73);
 let janeGrade = calculateStudentGrade(80);
 let jjGrade = calculateStudentGrade(60);
 let alexGrade = calculateStudentGrade(90);
-let aledxGrade = calculateStudentGrade(900);
 
 console.log(`John grade is ${johnGrade}`);
 console.log(`Lisa grade is ${lisaGrade}`);
 console.log(`Jane grade is ${janeGrade}`);
 console.log(`JJ grade is ${jjGrade}`);
 console.log(`Alex grade is ${alexGrade}`);
-console.log(`Alex grade is ${aledxGrade}`);
